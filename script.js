476341bb34f6cf383fb6190c55f0eac49135f315
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-08-12 16:21:38 +1000

    Persue button grid idea

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 gameBoard = {
   gameBoard: [1, 2, 2, 1, 2, 1, 2, 1, 1],
 };
+
+function displayGameState(array) {
+  for (i = 0; i < array.length; i++) {
+    //Add div with appropriare class to grid container
+  }
+}
