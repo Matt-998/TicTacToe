0b7ea4e0b8c999d5325bc032fe753f4acea2dd1d
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-08-13 13:20:36 +1000

    displayGameState working for now

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ gameBoard = {
   gameBoard: [1, 2, 2, 1, 2, 1, 2, 1, 1],
 };
 
+let container = document.querySelector(".grid");
+let buttons = container.children;
+console.log(buttons);
 function displayGameState(array) {
   for (i = 0; i < array.length; i++) {
-    //Add div with appropriare class to grid container
+    switch (array[i]) {
+      case 0:
+        buttons[i].className = "blank";
+        break;
+      case 1:
+        buttons[i].className = "token1";
+        break;
+      case 2:
+        buttons[i].className = "token2";
+        break;
+    }
   }
 }
+
+displayGameState(gameBoard.gameBoard);
