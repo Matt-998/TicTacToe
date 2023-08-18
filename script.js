4afc39cc0066882d97a82ef205b16b233cf0891c
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-08-18 11:15:23 +1000

    Get square claiming working

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-gameBoard = {
-  gameBoard: [1, 2, 2, 1, 2, 1, 2, 1, 1],
-};
-
-let container = document.querySelector(".grid");
-let buttons = container.children;
-function displayGameState(array) {
-  for (i = 0; i < array.length; i++) {
-    switch (array[i]) {
-      case 0:
-        buttons[i].className = "blank";
-        break;
-      case 1:
-        buttons[i].className = "token1";
-        break;
-      case 2:
-        buttons[i].className = "token2";
-        break;
+function gameBoard() {
+  let board = [1, 2, 2, 1, 2, 1, 2, 1, 1];
+  let container = document.querySelector(".grid");
+  let buttons = container.children;
+  const displayGameState = (board) => {
+    for (i = 0; i < board.length; i++) {
+      switch (board[i]) {
+        case 0:
+          buttons[i].className = "blank";
+          break;
+        case 1:
+          buttons[i].className = "token1";
+          break;
+        case 2:
+          buttons[i].className = "token2";
+          break;
+      }
     }
-  }
+  };
+  const getBoard = () => board;
+  return { displayGameState, getBoard };
 }
 
 function ModuleOfStuff() {
-  let currentPlayer = 1;
+  const board = gameBoard();
+  const players = [
+    {
+      name: "playerOne",
+      number: 1,
+    },
+    {
+      name: "playerTwo",
+      number: 2,
+    },
+  ];
+  let currentPlayer = players[0];
   const switchPlayer = () => {
     currentPlayer === 2 ? (currentPlayer = 1) : (currentPlayer = 2);
   };
@@ -29,36 +41,28 @@ function ModuleOfStuff() {
   const getCurrentPlayer = () => {
     return currentPlayer;
   };
-  return { switchPlayer, getCurrentPlayer };
+  const playRound = (index) => {
+    board.getBoard()[index] = currentPlayer;
+    switchPlayer();
+    board.displayGameState(board.getBoard());
+  };
+  return { switchPlayer, getCurrentPlayer, playRound };
 }
 
-function playRound() {
-  const Module = ModuleOfStuff();
+function MainModule() {
+  let container = document.querySelector(".grid");
+  let buttons = container.children;
+  const stuff = ModuleOfStuff();
+  const board = gameBoard();
+  function clickHandler(e) {
+    const selectedCell = e.target.dataset.index;
+    if (!selectedCell) return;
+    stuff.playRound(selectedCell);
+  }
 
-  //Get current player
-  //Pick spot
-  //Switch player
-  //Update board state
+  for (let button of buttons) {
+    button.addEventListener("click", clickHandler);
+  }
+  board.displayGameState(board.getBoard());
 }
-
-function clickHandler(e) {
-  const selectedCell = e.target.dataset.index;
-  if (!selectedCell) return;
-  //play round
-  //update screen
-}
-
-// add event listeners fo click handler function
-
-function selectSpot() {
-  const Module = ModuleOfStuff();
-  gameBoard.gameBoard[this.dataset.index] = Module.getCurrentPlayer();
-  Module.switchPlayer();
-  displayGameState(gameBoard.gameBoard);
-}
-
-for (let button of buttons) {
-  button.addEventListener("click", selectSpot);
-}
-ModuleOfStuff();
-displayGameState(gameBoard.gameBoard);
+MainModule();
