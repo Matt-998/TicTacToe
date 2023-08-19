c6ed3b3ef9a88f59bdc62fd74ae4750c1515f384
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-08-19 19:14:57 +1000

    Add win conditions and a checkCondition function

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const gameBoard = (function gameBoard() {
   return { displayGameState, getBoard, resetBoard };
 })();
 
-function ModuleOfStuff() {
+function GameController() {
   const board = gameBoard;
   const playerDisplay = document.querySelector(".currentPlayer");
   const players = [
@@ -41,36 +41,89 @@ function ModuleOfStuff() {
       number: 2,
     },
   ];
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   let currentPlayer = players[0].number;
+
   const displayCurrentPlayer = () => {
     playerDisplay.textContent = currentPlayer;
   };
+
   displayCurrentPlayer();
+
   const switchPlayer = () => {
     currentPlayer === 2 ? (currentPlayer = 1) : (currentPlayer = 2);
     displayCurrentPlayer();
   };
+
   const getCurrentPlayer = () => currentPlayer;
+
+  const checkCondition = (condition) => {
+    let result = "";
+    for (i = 0; i < 3; i++) {
+      if (board.getBoard()[condition[i]] === 0) return;
+      result += board.getBoard()[condition[i]];
+    }
+    if (result === "111" || result === "222") return true;
+    return false;
+  };
+
   const playRound = (index) => {
     if (board.getBoard()[index] !== 0) return;
     board.getBoard()[index] = currentPlayer;
-    switchPlayer();
     board.displayGameState(board.getBoard());
+    if (winConditions.some(checkCondition)) {
+      console.log(`${currentPlayer} is the winner`);
+    }
+    switchPlayer();
   };
+
   return { getCurrentPlayer, playRound };
 }
 
 function MainModule() {
   const buttons = document.querySelector(".grid").children;
-  const stuff = ModuleOfStuff();
+  const game = GameController();
   function clickHandler(e) {
     const selectedCell = e.target.dataset.index;
     if (!selectedCell) return;
 
-    stuff.playRound(selectedCell);
+    game.playRound(selectedCell);
   }
   for (let button of buttons) {
     button.addEventListener("click", clickHandler);
   }
 }
 MainModule();
+
+// let board = [1, 1, 1, 0, 0, 0, 0, 0, 0];
+// const winConditions = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6],
+// ];
+
+// const checkCondition = (condition) => {
+//   let result = "";
+//   for (i = 0; i < 3; i++) {
+//     if (board[condition[i]] === 0) return;
+//     result += board[condition[i]];
+//   }
+//   if (result === "111" || result === "222") return true;
+//   return false;
+// };
+
+// console.log(winConditions.some(checkCondition));
